18654ef185d0532c51e3c34c9bb15c0ad275f1c7
Shaggggy_ <79113250+jazdzewt@users.noreply.github.com>
2024-03-26 16:30:06 +0100

    Plik nagłówkowy algorytmu wall-follower

---
+++ wall_follower.h
@@ -0,0 +1,4 @@
+int if_exit (char **maze, int y, int x);
+int count (char **maze, int y, int x);
+void move_forward (char **maze, int *y, int *x, int *dir, int *dir_node);
+void wall_follower(char ***maze, int height, int width);
